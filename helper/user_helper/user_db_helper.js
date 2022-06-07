a9f426bc4ed755071841002bb650e68d6b5d157e
abhijith <abhijithelampal005@gmail.com>
2022-06-07 14:20:22 +0530

    modify singleshow

--- helper/user_helper/user_db_helper.js
+++ helper/user_helper/user_db_helper.js
@@ -2,6 +2,7 @@ const db = require('../../config/mongo.config')
 const { ObjectId } = require('mongodb')
 const coll = require('../collection')
 
+
 module.exports = {
     // all running movies
     runningMovies: () => {
@@ -26,6 +27,39 @@ module.exports = {
                 resolve(data);
             } else {
                 resolve(false);
+            }
+        })
+    },
+    singleShow: (obj) => {
+        return new Promise(async (resolve, reject) => {
+            let data = await db.getDb().collection(coll.movie).aggregate([
+                {
+                    $match: {
+                        "_id": ObjectId(obj)
+                    }
+                }
+            ]).toArray();
+            if (data) {
+                resolve(data)
+            } else {
+                resolve(false)
+            }
+        })
+    },
+    //show all theatre running movie
+    runningTheatre: (obj) => {
+        return new Promise(async (resolve, reject) => {
+            let data = await db.getDb().collection(coll.show).aggregate([
+                {
+                    $match: {
+                        "movieId": ObjectId(obj)
+                    }
+                }
+            ]).toArray()
+            if (data) {
+                resolve(data)
+            } else {
+                resolve(false)
             }
         })
     }
