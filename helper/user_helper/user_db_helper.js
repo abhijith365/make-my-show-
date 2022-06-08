c09ec2f839d49e4521b15f7e657f2bf317fe048a
abhijith <abhijithelampal005@gmail.com>
2022-06-08 23:06:24 +0530

    added filter function

--- helper/user_helper/user_db_helper.js
+++ helper/user_helper/user_db_helper.js
@@ -7,22 +7,30 @@ module.exports = {
     // all running movies
     runningMovies: () => {
         return new Promise(async (resolve, reject) => {
-            let data = await db.getDb().collection(coll.show).aggregate([
-                {
-                    $match: {
-                        "showByDate.endDate": { $gte: new Date() },
-                        "showType": "Movie"
-                    }
-                },
-                {
-                    $lookup: {
-                        from: 'movies',
-                        localField: 'movieId',
-                        foreignField: '_id',
-                        as: 'movies'
-                    }
-                }
-            ]).toArray();
+            let data = await db.getDb().collection(coll.show).aggregate(
+                [
+                    {
+                        '$match': {
+                            'showByDate.endDate': {
+                                '$gte': new Date()
+                            }
+                        }
+                    }, {
+                        '$lookup': {
+                            'from': 'movies',
+                            'localField': 'movieId',
+                            'foreignField': '_id',
+                            'as': 'movies'
+                        }
+                    }, {
+                        '$project': {
+                            'movies': 1,
+                            '_id': 0
+                        }
+                    },
+
+                ]
+            ).toArray();
             if (data) {
                 resolve(data);
             } else {
@@ -51,8 +59,39 @@ module.exports = {
         return new Promise(async (resolve, reject) => {
             let data = await db.getDb().collection(coll.show).aggregate([
                 {
-                    $match: {
-                        "movieId": ObjectId(obj)
+                    '$match': {
+                        'movieId': ObjectId(obj)
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'screens',
+                        'localField': 'screenId',
+                        'foreignField': '_id',
+                        'as': 'screens'
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'theatres',
+                        'localField': 'screens.theatreId',
+                        'foreignField': '_id',
+                        'as': 'theatres'
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'movies',
+                        'localField': 'movieId',
+                        'foreignField': '_id',
+                        'as': 'movies'
+                    }
+                }, {
+                    '$project': {
+                        '_id': 1,
+                        'showByDate': 1,
+                        'showByDay': 1,
+                        'totalShowDays': 1,
+                        'totalShowsInDay': 1,
+                        'theatres': 1,
+                        'movies': 1
                     }
                 }
             ]).toArray()
