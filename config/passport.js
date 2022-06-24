8f3f8f441663cb582e1eb08a0c1dbeb3691677de
abijith365 <abhijithelampal005@gmail.com>
2022-06-24 23:13:28 +0530

    code updated

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://abhijithv.xyz/auth/google/callback"
+        callbackURL: "/auth/google/callback"
     }, async (accessToken, refreshToken, profile, done) => {
 
         const newUser = {
